91de54348cacc37f3cdc11106ef8874b1b8121ce
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 18:03:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ end
   
 def num_points_scored(playername)
 playerpoints = 0 
-game_hash = game_hash
+game_hash[:home][:players].merge(game_hash[:away][:players])
  game_hash.collect do |team, stats|
   stats[:players].collect do |player, data|
 
