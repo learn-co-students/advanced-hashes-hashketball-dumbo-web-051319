1c21685735b7315ab943b412874992c8e60ae4b5
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 16:27:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,6 +118,7 @@ def game_hash
 end
 
 def num_points_scored(players_name)
+    binding.pry 
     game_hash.each do |location, team_data|
         team_data.each do |attribute, data|
             if data.include?(players_name)
